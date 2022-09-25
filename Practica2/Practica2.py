fb5acecf01b3cf38382a5cb4ab807a41591d27db
Brayan Rivera <brayan.israel99@gmail.com>
2022-09-24 20:29:41 -0500

    initial commit

--- Practica2/Practica2.py
+++ Practica2/Practica2.py
@@ -42,18 +42,18 @@ def mostrar_generos():
 
 def mostrar_estadistica_de_generos():
     df = pd.read_csv("csv's/typed_dataset.csv")
-    df_by_type = df.groupby(["Genero"]).agg({'Gross': ['sum', 'count', 'mean', 'max']})
-    df_by_type.reset_index(inplace=True)
-    df_by_type.columns = ['Genero', 'Ingresos_Totales_Genero', 'Pelis_Totales', 'Ingreso_Promedio', 'Ingreso_Maximo']
-    df_by_type.set_index("Genero", inplace=True)
-    print_tabulate(df_by_type.head(150))
+    df_por_genero = df.groupby(["Genero"]).agg({'Gross': ['sum', 'count', 'mean', 'max']})
+    df_por_genero.reset_index(inplace=True)
+    df_por_genero.columns = ['Genero', 'Ingresos_Totales_Genero', 'Pelis_Totales', 'Ingreso_Promedio', 'Ingreso_Maximo']
+    df_por_genero.set_index("Genero", inplace=True)
+    print_tabulate(df_por_genero.head(150))
 
 def mostrar_suma_ingresos_por_genero():
     df = pd.read_csv("csv's/typed_dataset.csv")
-    df_by_type = df.groupby(["Genero"])[["Gross"]].aggregate(pd.DataFrame.sum)
-    df_by_type.reset_index(inplace=True)
-    df_by_type.set_index("Genero", inplace=True)
-    print_tabulate(df_by_type.head(150))
+    df_por_genero = df.groupby(["Genero"])[["Gross"]].aggregate(pd.DataFrame.sum)
+    df_por_genero.reset_index(inplace=True)
+    df_por_genero.set_index("Genero", inplace=True)
+    print_tabulate(df_por_genero.head(150))
 
 ##########################################################################################################################
 
--- Practica3/Practica3.py
+++ Practica3/Practica3.py
@@ -1,15 +1,14 @@
 import pandas as pd
 from tabulate import tabulate
-from typing import Tuple, List
 import matplotlib.pyplot as plt
 
 def print_tabulate(df: pd.DataFrame):
     print(tabulate(df, headers=df.columns, tablefmt='orgtbl'))
 
-def create_boxplot_by_type(file_name:str, column: str, agg_fn= pd.DataFrame.sum):
-    df_complete = pd.read_csv(file_name)
-    df_by_type = df_complete.groupby([column,"Fecha"])[["Gross"]].aggregate(agg_fn)# .count()
-    df_by_type.boxplot(by = column, figsize=(27,18))
+def crear_grafica_de_generos(file_name:str, column: str, agg_fn= pd.DataFrame.sum):
+    df = pd.read_csv(file_name)
+    df_por_genero = df.groupby([column,"Fecha"])[["Gross"]].aggregate(agg_fn)
+    df_por_genero.boxplot(by = column, figsize=(27,18))
     plt.xticks(rotation=90)
     plt.savefig(f"img/boxplot_{column}.png")
     plt.close()
@@ -20,19 +19,19 @@ def plot_by_dep(df: pd.DataFrame, dep:str)->None:
     df[df["Genero"] == dep].boxplot(by ='Genero')
     plt.savefig(f"img/bplt/bplt_{dep}.png")
 
-def create_plot_por_Genero(file_name:str):
-    df_complete = pd.read_csv(file_name)
-    df_by_dep = df_complete.groupby(["Genero", "Fecha"])[["Gross"]].aggregate(pd.DataFrame.mean)
-    df_by_dep.reset_index(inplace=True)
-    df_by_dep.set_index("Fecha", inplace=True)
+def crear_grafica_de_genero_en_especifico(file_name:str):
+    df = pd.read_csv(file_name)
+    df_por_genero = df.groupby(["Genero", "Fecha"])[["Gross"]].aggregate(pd.DataFrame.mean)
+    df_por_genero.reset_index(inplace=True)
+    df_por_genero.set_index("Fecha", inplace=True)
 
-    for dep in set(df_by_dep["Genero"]):
-       plot_by_dep(df_by_dep, dep)
-    df_aux = df_complete.groupby(["Fecha","Genero"])[['Gross']].mean().unstack()
+    for dep in set(df_por_genero["Genero"]):
+       plot_by_dep(df_por_genero, dep)
+    df_aux = df.groupby(["Fecha","Genero"])[['Gross']].mean().unstack()
     df_aux.plot(y = 'Gross', legend=False, figsize=(32,18))
     plt.xticks(rotation=90)
     plt.savefig("img/foo.png")
     plt.close()
 
-# create_boxplot_by_type("csv's/typed_dataset.csv", 'Genero', pd.DataFrame.mean)
-# create_plot_por_Genero("csv's/typed_dataset.csv")
+crear_grafica_de_generos("csv's/typed_dataset.csv", 'Genero', pd.DataFrame.mean)
+crear_grafica_de_genero_en_especifico("csv's/typed_dataset.csv")
